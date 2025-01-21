8f3d8ed89b0b54499384717fecf6d672b7bf4514
Anniel-Reyes <areyes@brisiao.do>
2025-01-21 15:42:23 -0400

    fix: adding app.listen

--- src/index.ts
+++ src/index.ts
@@ -21,7 +21,8 @@ app.use(cors());
 app.use(express.json());
 app.use("/api", routes);
 
-
-
+app.listen(process.env.PORT, () => {
+  console.log(`Server is running on port ${process.env.PORT}`);
+});
 
 export default app;
