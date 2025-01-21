6edc81f21e24368701054170f4f979feaf231b54
Anniel-Reyes <areyes@brisiao.do>
2025-01-21 16:06:35 -0400

    fix: import dotenv

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,19 @@
 import express from "express";
-import dotenv from "dotenv";
+import 'dotenv/config'
 import cors, { CorsOptions } from "cors";
 import routes from "./routes";
-dotenv.config();
+
 const app: express.Application = express();
-const urlList = ["http://localhost:5173", "https://fs-daycare.netlify.app"];
-const corsOptions: CorsOptions = {
-  origin: function (origin, callback) {
-    if (origin && urlList.indexOf(origin) !== -1) {
-      callback(null, true);
-    } else {
-      callback(new Error("Not allowed by CORS"));
-    }
-  },
-};
+//const urlList = ["http://localhost:5173", "https://fs-daycare.netlify.app"];
+// const corsOptions: CorsOptions = {
+//   origin: function (origin, callback) {
+//     if (origin && urlList.indexOf(origin) !== -1) {
+//       callback(null, true);
+//     } else {
+//       callback(new Error("Not allowed by CORS"));
+//     }
+//   },
+// };
 app.use(cors());
 app.use(express.json());
 app.use("/api", routes);
