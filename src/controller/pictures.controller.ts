810dc5e165b06ac1cc8221c463fe2c3e1b99b23a
Anniel-Reyes <areyes@brisiao.do>
2025-01-22 19:49:33 -0400

    style: removing comments

--- src/controller/pictures.controller.ts
+++ src/controller/pictures.controller.ts
@@ -36,38 +36,6 @@ async function getLatestPictures(req: Request, res: Response) {
   }
 }
 
-// function postPictures(req: Request, res: Response) {
-//   try {
-//     upload.single("file")(req, res, async (err) => {
-//       if (err) {
-//         return res
-//           .status(400)
-//           .json({ error: err, message: "Error uploading file." });
-//       }
-
-//       const image = req.file;
-//       if (image) {
-//         try {
-//           const result = await cloudinary.v2.uploader.upload(image.path, {
-//             folder: "",
-//             resource_type: "image",
-//           });
-//           res
-//             .status(200)
-//             .json({ message: "File uploaded successfully.", result });
-//         } catch (error: any) {
-//           res
-//             .status(500)
-//             .json({ error, message: "Error uploading to Cloudinary." });
-//         }
-//       } else {
-//         res.status(400).json({ message: "No file uploaded." });
-//       }
-//     });
-//   } catch (error: any) {
-//     res.status(500).json({ error: error });
-//   }
-// }
 async function postPictures(req: Request, res: Response) {
   upload.single("file")(req, res, async (err) => {
     if (err) {
