626a25ad6a8880fb81261f566addc38932d52310
Anniel-Reyes <areyes@brisiao.do>
2025-01-22 14:25:29 -0400

    fix: now getPictures return 2000 pictures

--- src/controller/pictures.controller.ts
+++ src/controller/pictures.controller.ts
@@ -8,6 +8,7 @@ async function getPictures(req: Request, res: Response) {
       type: "upload",
       prefix: "",
       resource_type: "image",
+      max_results: 2000,
     });
     res.status(200).json({ images: result.resources });
   } catch (error) {
